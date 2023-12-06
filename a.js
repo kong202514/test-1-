566049d702e1218b0dccb89df323691157b0b4d9
kong202514 <0635890449gong@gmail.com>
2023-12-06 12:11:41 +0700

    q

--- a.js
+++ a.js
@@ -1,22 +1,22 @@
 "use strict";
-const x = "hello";
-console.log(x.substring(1)); //ello
-console.log(x.charAt(0)); //h
-function reverseString(text) {
-    let reverseText = "";
-    for (let i = text.length - 1; i >= 0; i--) {
-        reverseText += text[i];
-    }
-    return reverseText;
-}
-console.log(reverseString(x)); //olleh
-// function recursiveReverseString(text: string) {
-//     return ""
+// const x = "hello";
+// console.log(x.substring(1)); //ello
+// console.log(x.charAt(0)); //h
+// function reverseString(text) {
+//     let reverseText = "";
+//     for (let i = text.length - 1; i >= 0; i--) {
+//         reverseText += text[i];
+//     }
+//     return reverseText;
 // }
-function recursiveReverseString(text) {
-    if (text.length <= 0)
-        return text;
-    return recursiveReverseString(text.substring(1)) + text.charAt(0);
-}
-console.log(recursiveReverseString('hello')); //olleh
-console.log(recursiveReverseString('prayuth')); //htuyarp
+// console.log(reverseString(x)); //olleh
+// // function recursiveReverseString(text: string) {
+// //     return ""
+// // }
+// function recursiveReverseString(text) {
+//     if (text.length <= 0)
+//         return text;
+//     return recursiveReverseString(text.substring(1)) + text.charAt(0);
+// }
+// console.log(recursiveReverseString('hello')); //olleh
+// console.log(recursiveReverseString('prayuth')); //htuyarp
